290f209bed709bcc786758de7f95b5abd782d4c6
suzuharu0920 <sz.haruto0920@icloud.com>
2021-06-22 17:24:46 +0900

    マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -56,15 +56,15 @@ ActiveRecord::Schema.define(version: 2021_06_20_154301) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.text "profile"
-    t.text "occupation"
-    t.text "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
